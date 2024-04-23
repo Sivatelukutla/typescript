c5559827100e0817418d7fd317fa1abff7598412
Sivatelukutla <sivatelukutla123@gmail.com>
2024-04-23 15:28:13 +0530

    type alias

---
+++ type.js
@@ -0,0 +1,7 @@
+var a;
+var b;
+var c;
+a = "siva";
+b = 10;
+c = 22;
+//here it's come error because in c variable assign a string and boolean type not a number type
---
+++ type.ts
@@ -0,0 +1,12 @@
+type t1 = string | number;
+type t2 = number | boolean;
+type t3 = string | boolean;
+
+let a : t1;
+let b : t2; 
+let c : t3;
+
+a = "siva";
+b = 10;
+c = 22;
+//here it's come error because in c variable assign a string and boolean type not a number type
