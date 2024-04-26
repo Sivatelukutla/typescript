881dbe82f00a7ab163fb5c6c1160b6f9a13417c9
Sivatelukutla <sivatelukutla123@gmail.com>
2024-04-26 21:18:01 +0530

    basics a typescript

---
+++ typescriptbasic/basic.ts
@@ -0,0 +1,3 @@
+var a = "siva";
+a = 22; //here its comes error (because a variable assign a string type not a number type);
+console.log(a); //its called a infressing type => default take value of date type
