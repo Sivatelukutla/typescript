2ce55d2e304441ab8d6d81da3685f4b6d201abc5
Sivatelukutla <sivatelukutla123@gmail.com>
2024-04-27 11:22:37 +0530

    array topic

---
+++ typescriptbasic/array.ts
@@ -0,0 +1,18 @@
+var myArray = [];
+//myArray.push(2); // here its come error because myArray variable assign a never type (in that myarray variable no type of value assign)
+//so we have to give a data type (see the below code)
+var myArray1 : number[] = [];
+myArray1.push(22);
+//myArray1.push("siva"); //here its come error in myArray1 variable assign a only number type not a string type 
+//so we has to give string type also (see the below code)
+var myArray2 : number[] | string[] = [];//(|=> this symbool is called a union type)
+myArray2 = [22, 43, 55, 66];
+myArray2 = ["prasad", "pavan", "hyderbad"];
+//myArray2 = [33, 55, 77, "rakesh", "car", "parrot"];//here its error (myArray2 variable is not assign a combination of number , string type)
+//in the above code we have to write another way (its see)
+var myArray3 : (number|string)[] = []; 
+myArray3 = [22, 55, 66, 7, 6, "siva", "prasada", "hen", "pen"];
+
+
+export{}
+
