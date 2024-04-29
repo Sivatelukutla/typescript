e85cec2cfbdc612bdd0eb55a43d3cd50aaf5a729
Sivatelukutla <sivatelukutla123@gmail.com>
2024-04-29 22:29:28 +0530

    interface

---
+++ typescriptbasic/interface.js
@@ -0,0 +1,11 @@
+//a feature of TypeScript that allows us to define the structure or shape of an
+// object and specify the properties and methods that an object has or should have.
+var Details = {
+    name: "siva",
+    age: 22,
+    address: "Agraharam",
+    colleage: "akvk",
+    getOf: function () {
+        return "prasad";
+    }
+};
