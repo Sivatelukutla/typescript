a99a2afe9b5afbc09f5401d054b84722c8f8a316
Sivatelukutla <sivatelukutla123@gmail.com>
2024-05-01 18:53:14 +0530

    classes

---
+++ typescriptbasic/class.js
@@ -0,0 +1,10 @@
+//classes 
+var PersonDetails = /** @class */ (function () {
+    function PersonDetails(name, age, city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+    return PersonDetails;
+}());
+var reslut = new PersonDetails("rakesh", 22, "Ongole");
