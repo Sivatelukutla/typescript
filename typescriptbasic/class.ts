cae200df53a65df4c7b3649be245b312a2809744
Sivatelukutla <sivatelukutla123@gmail.com>
2024-05-01 18:55:13 +0530

    classes

---
+++ typescriptbasic/class.ts
@@ -0,0 +1,14 @@
+//classes 
+
+class PersonDetails{
+    name : string
+    age : number 
+    city : string
+    constructor(name:string, age:number, city:string){
+        this.name = name;
+        this.age = age;
+        this.city = city
+    }
+}
+
+const reslut = new PersonDetails("rakesh", 22, "Ongole")
