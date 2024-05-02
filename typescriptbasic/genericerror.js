6995dfc075c7a079934d3fe9d4bbb1608211d15d
Sivatelukutla <sivatelukutla123@gmail.com>
2024-05-02 15:46:31 +0530

    errorgeneric

---
+++ typescriptbasic/genericerror.js
@@ -0,0 +1,5 @@
+//how to access the array in error function 
+var resultSum = function (value) {
+    var sums = 4;
+    return value[sums];
+};
---
+++ typescriptbasic/genericerror.ts
@@ -0,0 +1,7 @@
+//how to access the array in error function 
+
+const resultSum = <T,>(value:T[]):T => {
+    const sums = 4;
+    return value[sums];
+}
+
