bf505bf119a72bb287df181e598726d31e678f74
Sivatelukutla <sivatelukutla123@gmail.com>
2024-05-22 06:38:27 +0530

    objectmethods

--- typescriptbasic/object3.js
+++ typescriptbasic/object3.js
@@ -27,3 +27,76 @@ console.log(Object.values(obj1));  // it's returns a array => [ 'siva', 24, 9392
 
 const obj2 = { foo: "bar", baz: 42 };
 console.log(Object.entries(obj2)); // [ ['foo', 'bar'], ['baz', 42] ]
+
+//Object.freeze()
+
+var obj3 = {
+    name : "siva",
+    age : 45,
+    city : "podili"
+}
+
+Object.freeze(obj3);
+
+//onces here freeze a any object now onwards those not change or add or remove on obj 
+
+obj3.name = "prasad";
+console.log(obj3.name);//siva
+
+//Object.fromEntries()
+//The Object.fromEntries() static method transforms a list of key-value pairs into an object.
+
+const entries = new Map([
+    ['foo', 'bar'],
+    ['baz', 42],
+]);
+  
+const obj4 = Object.fromEntries(entries);
+  
+console.log(obj4);
+// Expected output: Object { foo: "bar", baz: 42 }
+
+const arr = [
+    ["0", "a"],
+    ["1", "b"],
+    ["2", "c"],
+];
+const obj5 = Object.fromEntries(arr);
+console.log(obj5); // { 0: "a", 1: "b", 2: "c" }
+
+const object1 = { a: 1, b: 2, c: 3 };
+
+const object2 = Object.fromEntries(
+  Object.entries(object1).map(([key, val]) => [key, val * 2]),
+);
+
+console.log(object2);
+// { a: 2, b: 4, c: 6 }
+
+//Object.getOwnPropertyDescriptor()
+
+//The Object.getOwnPropertyDescriptor() static method returns an object describing the configuration of a specific property on a given object
+
+const object3 = {
+    property1: 42,
+  };
+  
+const descriptor1 = Object.getOwnPropertyDescriptor(object3, 'property1');
+  
+console.log(descriptor1.configurable);
+  // Expected output: true
+  
+console.log(descriptor1.value);
+  // Expected output: 42
+  
+//Object.getOwnPropertyNames()
+//static method returns an array of all properties
+const object4 = {
+    a: 1,
+    b: 2,
+    c: 3,
+};
+  
+console.log(Object.getOwnPropertyNames(object4));
+// Expected output: Array ["a", "b", "c"]
+  
